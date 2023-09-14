8551bdce551a794801038a1bc12a384f6db015b8
julgiraldoheinsohn <julgiraldo@heinsohn.com.co>
2023-09-14 14:07:08 -0500

    Add navbar completely

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,71 +1,108 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-    <nav>
-        /** Left side */
-        <ul>
-            <li>
-                <NavLink to='/'>
-                    Shop
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/'>
-                    All
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/clothes'>
-                    Clothes
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/electronics'>
-                    Electronics
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/fornitures'>
-                    Furnitures
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/toys'>
-                    Toys
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/others'>
-                    Others
-                </NavLink>
-            </li>
-        </ul>
+  const activeStyle = 'underline underline-offset-4'
 
-        /** Rigth side */
-        <ul>
-            <li>
-                julian09426@gmail.com
-            </li>
-            <li>
-                <NavLink to='/my-orders'>
-                    My Orders
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/my-account'>
-                    My Account
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/sign-in'>
-                    Sign in
-                </NavLink>
-            </li>
-            <li>
-                Cart
-            </li>
-        </ul>
+  return (
+    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to='/'>
+            Shopi
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/clothes'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Clothes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/electronics'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Electronics
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/furnitures'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Furnitures
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/toys'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Toys
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/others'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Others
+          </NavLink>
+        </li>
+      </ul>
+      <ul className='flex items-center gap-3'>
+        <li className='text-black/60'>
+          teff@platzi.com
+        </li>
+        <li>
+          <NavLink
+            to='/my-orders'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/my-account'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/sing-in'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Sign In
+          </NavLink>
+        </li>
+        <li>
+          🛒 0
+        </li>
+      </ul>
     </nav>
+  )
 }
 
 export default Navbar
